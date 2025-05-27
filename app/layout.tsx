4fc121bb88fe40a3fc2e62e73d408713dd8c1221
lwev <106478077+lwev@users.noreply.github.com>
2025-05-27 16:02:28 +0200

    “update4”

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Voice Grocery',
+  description: 'Create Shopping Lists by Voice',
+  generator: 'lweb.ch',
 }
 
 export default function RootLayout({
